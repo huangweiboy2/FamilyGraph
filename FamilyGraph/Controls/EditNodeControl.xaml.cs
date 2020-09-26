d438f50b3c99b2b9ca01ffc45d32586b50b8f167
SalHe <SalHe@qq.com>
2020-09-26 12:26:36 +0800

    提取信息编辑区为单独UserControl

---
+++ FamilyGraph/Controls/EditNodeControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace FamilyGraph.Controls
+{
+    public partial class EditNodeControl : UserControl
+    {
+        public EditNodeControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
