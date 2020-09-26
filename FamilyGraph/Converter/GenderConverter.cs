1e5f4c4c140dcb00a0e0e786f02f6f1bf5768f8e
SalHe <SalHe@qq.com>
2020-09-26 10:56:01 +0800

    完成节点编辑功能

---
+++ FamilyGraph/Converter/GenderConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using FamilyGraph.Internal;
+
+namespace FamilyGraph.Converter
+{
+    public class GenderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Enum.Parse(typeof(Gender), value as string ?? "Male");
+        }
+    }
+}
